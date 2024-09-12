62e7ca3b1e66ade3ea7699814678702c8de88694
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-12 15:11:28 +0200

    11. More on the Image Component
    -u app/page.tsx fajlu, komponenti Image dodana visina i sirina

--- corp/src/app/page.tsx
+++ corp/src/app/page.tsx
@@ -1,15 +1,23 @@
 import Image from "next/image"
 import homeImg from '/public/home.jpg'
+console.log(homeImg)
 
 export default function Home() {
   return (
       <div>
         Home Page
         <div className="absolute -z-10 inset-0">
-          <Image 
+          {/* <Image 
             src={homeImg} 
             alt='car factory'
             fill
+            style={{ objectFit: 'cover' }}
+          /> */}
+          <Image 
+            width={600}
+            height={800}
+            src={homeImg} 
+            alt='car factory'
             style={{ objectFit: 'cover' }}
           />
         </div>
