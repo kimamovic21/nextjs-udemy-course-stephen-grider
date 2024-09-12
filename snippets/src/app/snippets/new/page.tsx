385f088b83b41823fb0ebeff25b60c3bd5d78386
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-12 17:39:59 +0200

    21. Adding a Creation Form
    -u layout.tsx fajlu kreiran div element u koji je dodan prop children
    -u page.tsx fajlu kreirani HTML elementi za formu

--- snippets/src/app/snippets/new/page.tsx
+++ snippets/src/app/snippets/new/page.tsx
@@ -1,7 +1,35 @@
 export default function SnippetCreatePage() {
     return (
-        <div>
-            Create a Snippet!
-        </div>
+        <form>
+            <h3 className='font-bold m-3'>Create a Snippet</h3>
+            <div className='flex flex-col gap-4'>
+                <div className='flex gap-4'>
+                    <label className='w-12' htmlFor='title'>
+                        Title
+                    </label>
+                    <input 
+                        name='title'
+                        id='title'
+                        type='text'
+                        className='border rounded p-2 w-full'
+                    />
+                </div>
+
+                <div className='flex gap-4'>
+                    <label className='w-12' htmlFor='code'>
+                        Code
+                    </label>
+                    <textarea
+                        name='code'
+                        id='code'
+                        className='border rounded p-2 w-full'
+                    />
+                </div>
+
+                <button type='submit' className='rounded p-2 bg-blue-200'>
+                    Create
+                </button>
+            </div>
+        </form>
     )
 }
