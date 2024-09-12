d0226fc2fec6700aa6bc5c7f485a2ebd99b9f85b
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-12 15:31:13 +0200

    12. Adding a Reusable Presentation Component
    -u folderu components kreirana komponenta hero.tsx
    -u app/page.tsx fajl dodana komponenta hero.tsx

--- corp/src/app/page.tsx
+++ corp/src/app/page.tsx
@@ -1,26 +1,12 @@
-import Image from "next/image"
 import homeImg from '/public/home.jpg'
-console.log(homeImg)
+import Hero from '@/components/hero'
 
 export default function Home() {
   return (
-      <div>
-        Home Page
-        <div className="absolute -z-10 inset-0">
-          {/* <Image 
-            src={homeImg} 
-            alt='car factory'
-            fill
-            style={{ objectFit: 'cover' }}
-          /> */}
-          <Image 
-            width={600}
-            height={800}
-            src={homeImg} 
-            alt='car factory'
-            style={{ objectFit: 'cover' }}
-          />
-        </div>
-      </div>
+      <Hero 
+        imgData={homeImg} 
+        imgAlt='car factory'
+        title='Professional Cloud Hosting'
+      />
   )
 }
