8e165eba1841b595a6659895451881a20aa4350d
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-12 14:37:57 +0200

    07. Project Structure Strategy
    -kreiran folder components i header.tsx komponenta
    -u layout.tsx fajl dodana komponenta header.tsx

--- corp/src/app/layout.tsx
+++ corp/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Link from "next/link";
+import Header from '../components/header';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,12 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <div>
-          <Link href='/'>Home</Link>
-          <Link href='/performance'>Performance</Link>
-          <Link href='/reliability'>Reliability</Link>
-          <Link href='/scale'>Scale</Link>
-        </div>
+        <Header />
         {children}
       </body>
     </html>
