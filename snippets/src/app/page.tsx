c26bb7aa1797688f64b4baf5ad51a1a7fb359dbd
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-13 16:55:17 +0200

    25. Fetching Data with Server Components
    -u app/page.tsx fajl dodana logika za dohvacanje podataka iz baze

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
+import { db } from "@/db"
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany()
+
+  const renderedSnippets = snippets.map((snippet) => {
+    return (
+      <div key={snippet.id}>
+        <h2>{snippet.title}</h2>
+      </div>
+    )
+  })
+
   return (
     <div>
-      Home Page
+      {renderedSnippets}
     </div>
   )
 }
