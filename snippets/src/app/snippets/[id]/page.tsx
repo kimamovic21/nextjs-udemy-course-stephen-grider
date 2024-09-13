2c0f6b7377ada2c16bfe098dff48279e3e3fbe4d
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-13 17:17:10 +0200

    27. Fetching Particular Records
    -u [id]/page.tsx fajl dodana logika za prikazivanje podataka o jednoj biljeski

--- snippets/src/app/snippets/[id]/page.tsx
+++ snippets/src/app/snippets/[id]/page.tsx
@@ -1,7 +1,26 @@
-export default function SnippetShowPage(props: any) {
-  console.log('props: ', props);
+import { notFound } from 'next/navigation'
+import { db } from '@/db'
+
+interface SnippetShowPageProps {
+  params: {
+    id: string
+  } 
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  console.log('props: ', props)
+
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(props.params.id) }
+  })
+
+  if (!snippet) {
+    return notFound()
+  }
 
   return (
-    <div>SnippetShowPage</div>
+    <div>
+      <h2>{snippet.title}</h2>
+    </div>
   )
 }
