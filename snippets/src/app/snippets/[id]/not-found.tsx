f6efa6f1fb66ecc37c8f68ccab4be65b8bc64130
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-14 12:38:36 +0200

    28. Custom Not Found Pages
    -u folderu snippets/[id] kreiran not-found.tsx fajl

---
+++ snippets/src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+export default function SnippetNotFound() {
+  return (
+    <div>
+        <h2 className="text-xl bold">
+            Sorry, but we could not find that particular snippet!
+        </h2>
+    </div>
+  )
+}
