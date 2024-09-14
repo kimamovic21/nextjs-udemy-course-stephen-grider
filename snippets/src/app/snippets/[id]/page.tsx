49c50c6e902038128fa6e21b499cf893e21ce044
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-14 14:33:02 +0200

    32. Linking to the Edit Page
    -u folderu [id] kreiran folder edit i page.tsx fajl
    -u snippets/[id]/page.tsx fajl dodana komponenta Link

--- snippets/src/app/snippets/[id]/page.tsx
+++ snippets/src/app/snippets/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation'
 import { db } from '@/db'
+import Link from 'next/link'
 
 interface SnippetShowPageProps {
   params: {
@@ -25,7 +26,12 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
       <div className='flex m-4 justify-between items-center'>
         <h2 className='text-xl font-bold'>{snippet.title}</h2>
         <div className='flex gap-4'>
-          <button className='p-2 border rounded'>Edit</button>
+          <Link 
+            href={`/snippets/${snippet.id}/edit`} 
+            className='p-2 border rounded'
+          >
+            Edit
+          </Link>
           <button className='p-2 border rounded'>Delete</button>
         </div>
       </div>
