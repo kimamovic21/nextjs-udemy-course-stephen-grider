81503f5077e3e6cacc3d39e8932f6d40e0a20b05
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-16 12:01:14 +0200

    40. Deleting a Record with a Server Action
    -u actions/index.ts fajlu kreirana funkcija deleteSnippet()
    -u snippets/[id]/page.tsx fajl dodana logika za brisanje biljeske
    -manje izmjene u folderu prisma

--- snippets/src/app/snippets/[id]/page.tsx
+++ snippets/src/app/snippets/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation'
 import { db } from '@/db'
+import { deleteSnippet } from '@/actions'
 import Link from 'next/link'
 
 interface SnippetShowPageProps {
@@ -21,6 +22,8 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     return notFound()
   }
 
+  const deleteSnippetAction = deleteSnippet.bind(null, snippet.id)
+
   return (
     <div>
       <div className='flex m-4 justify-between items-center'>
@@ -32,7 +35,11 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
           >
             Edit
           </Link>
-          <button className='p-2 border rounded'>Delete</button>
+          <form action={deleteSnippetAction}>
+            <button className='p-2 border rounded'>
+              Delete
+            </button>
+          </form>
         </div>
       </div>
       <pre className='p-3 border rounded bg-gray-200 border-gray-200'>
