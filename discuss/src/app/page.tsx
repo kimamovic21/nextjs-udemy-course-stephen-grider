d1036c4d3c11febd6f91ed43a82d16d1b28daf4b
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-18 17:25:35 +0200

    62. Sign In, Sign Out and Checking Auth Status
    -morao sam instalirati ponovo Next.js aplikaciju verziju 14.0.2
    -izbrisan Next.js projekt verzija 14.2.12
    -manje izmjene u konfiguracionim fajlovima
    -manje izmjene u folderu prisma
    -kreiran folder public sa slikama
    -u folderu src kreiran folder components i komponenta Profile.tsx
    -u app/page.tsx fajl dodana komponenta Profile.tsx

--- discuss/src/app/page.tsx
+++ discuss/src/app/page.tsx
@@ -1,9 +1,32 @@
+import * as actions from '@/actions'
 import { Button } from '@nextui-org/react'
+import { auth } from '@/auth'
+import Profile from '@/components/Profile'
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
+
   return (
     <div>
-      <Button>Click me !!!</Button>
+      <form action={actions.signIn}>
+        <Button type='submit'>
+          Sign In
+        </Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type='submit'>
+          Sign Out
+        </Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Signed Out</div>
+      )}
+
+      <Profile />
     </div>
-  );
+  )
 }
