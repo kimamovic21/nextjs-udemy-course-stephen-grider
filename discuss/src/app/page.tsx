72cfa7bed4ef92d16586988093cc08039610afd7
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-21 15:30:42 +0200

    72. More Caching Issues
    -u app/page.tsx fajlu izbrisana logika u jsx-u
    -manje izmjene u folderu prisma

--- discuss/src/app/page.tsx
+++ discuss/src/app/page.tsx
@@ -1,32 +1,5 @@
-import * as actions from '@/actions'
-import { Button } from '@nextui-org/react'
-import { auth } from '@/auth'
-import Profile from '@/components/Profile'
-
-export default async function Home() {
-  const session = await auth()
-
+export default function Home() {
   return (
-    <div>
-      <form action={actions.signIn}>
-        <Button type='submit'>
-          Sign In
-        </Button>
-      </form>
-
-      <form action={actions.signOut}>
-        <Button type='submit'>
-          Sign Out
-        </Button>
-      </form>
-
-      {session?.user ? (
-        <div>{JSON.stringify(session.user)}</div>
-      ) : (
-        <div>Signed Out</div>
-      )}
-
-      <Profile />
-    </div>
+    <div>Home Page</div>
   )
 }
