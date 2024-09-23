1e1edb59c6cec57aa02b9ee87df22929a0257010
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-23 15:24:54 +0200

    78. Adding Form Validation with Zod
    -u terminalu instaliran paket Zod
    -manje izmjene u fajlovima package.json i package-lock.json
    -u create-topic.ts fajlu, u funkciju createTopic() dodana logika za validaciju unesenih vrijednosti za varijable name i description

--- discuss/src/actions/create-topic.ts
+++ discuss/src/actions/create-topic.ts
@@ -1,10 +1,28 @@
 'use server';
 
+import { z } from 'zod';
+
+const createTopicSchema = z.object({
+    name: z
+        .string()
+        .min(3)
+        .regex(/^[a-z-]+$/, { 
+            message: 'Must be lowercase letters or dashes without spaces!' 
+        }),
+    description: z
+        .string()
+        .min(10),
+})
+
 export async function createTopic(formData: FormData) {
-    const name = formData.get('name')
-    const description = formData.get('description')
-    console.log('name: ', name)
-    console.log('description: ', description)
+    const result = createTopicSchema.safeParse({
+        name: formData.get('name'),
+        description: formData.get('description'),
+    });
+
+    if (!result.success) {
+        console.error(result.error.flatten().fieldErrors);
+    };
 
     // TODO: revalidate the homepage 
-}
+};
