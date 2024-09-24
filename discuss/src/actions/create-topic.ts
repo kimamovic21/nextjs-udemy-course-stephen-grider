9a4f9f160f136a2379e4fdcbaf1075e0546cd420
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-24 14:48:46 +0200

    84. Handling Database Errors in Forms
    -u create-topic.ts fajl dodana logika za kreiranje teme
    -manje izmjene u folderu prisma

--- discuss/src/actions/create-topic.ts
+++ discuss/src/actions/create-topic.ts
@@ -1,7 +1,12 @@
 'use server';
 
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
 import { z } from 'zod';
 import { auth } from '@/auth';
+import type { Topic } from '@prisma/client';
+import { db } from '@/db';
+import paths from '@/paths';
 
 const createTopicSchema = z.object({
     name: z
@@ -45,12 +50,33 @@ export async function createTopic(
             errors: {
                 _form: ['You must be signed in to create a topic!'],
             }
-        }
-    }
-
-    return {
-        errors: {}
+        };
     };
 
-    // TODO: revalidate the homepage 
+    let topic: Topic;
+    try {
+        topic = await db.topic.create({
+            data: {
+                slug: result.data.name,
+                description: result.data.description
+            },
+        });
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            return {
+                errors: {
+                    _form: [err.message],
+                },
+            };
+        } else {
+            return {
+                errors: {
+                    _form: ['Something went wrong!'],
+                },
+            };
+        };
+    };
+
+    revalidatePath('/');
+    redirect(paths.topicShow(topic.slug));
 };
