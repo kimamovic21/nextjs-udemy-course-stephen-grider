5cc39fafa7fdde6503d85863ab6a9bc882ee9f58
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-24 17:01:52 +0200

    87. Scaffolding the Topic Show Page
    -u [slug]/page.tsx fajl dodana logika za prikazivanje naslova teme
    -manje izmjene u folderu prisma

--- discuss/src/app/topics/[slug]/page.tsx
+++ discuss/src/app/topics/[slug]/page.tsx
@@ -1,5 +1,19 @@
-export default function TopicShowPage() {
+interface TopicShowPageProps {
+  params: {
+    slug: string;
+  }
+}
+
+export default function TopicShowPage({ params }: TopicShowPageProps) {
+  const { slug } = params
+
   return (
-    <div>TopicShowPage</div>
+    <div className='grid grid-cols-4 gap-4 p-4'>
+      <div className='col-span-3'>
+        <h2 className='text-2xl font-bold mb-2'>
+          {slug}
+        </h2>
+      </div>
+    </div>
   )
 }
