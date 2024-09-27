e2174f263d1f38847a83e061bf7b93da3c7b8066
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-27 14:57:16 +0200

    115. Redirecting From a Server Action
    -u folderu actions kreiran search.ts fajl i funkcija search()
    -u index.ts fajlu eksportovana funkcija search
    -u komponentu SearchInput.tsx dodana funkcija search

--- discuss/src/components/SearchInput.tsx
+++ discuss/src/components/SearchInput.tsx
@@ -2,11 +2,17 @@
 
 import { Input } from '@nextui-org/react'
 import { useSearchParams } from 'next/navigation'
+import * as actions from '@/actions'
 
 export default function SearchInput() {
   const searchParams = useSearchParams()
 
   return (
-    <Input defaultValue={searchParams.get('term') || ''} />
+    <form action={actions.search}>
+      <Input 
+        name='term'
+        defaultValue={searchParams.get('term') || ''} 
+      />
+    </form>
   )
 }
