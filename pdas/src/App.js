62075c2818ea38835802b14f982fdab071d88f79
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-24 11:31:43 +0200

    148. Including Images
    -u folderu src dodan folder images sa slikama
    -u komponentu App.js dodan import za slike

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,9 +1,18 @@
 import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+
+console.log(AlexaImage);
+console.log(CortanaImage);
+console.log(SiriImage);
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
+
+      <img src="https://picsum.photos/200/300" alt='random pic' />
 
       <ProfileCard title="Alexa" handle="@alexa99" />
       <ProfileCard title="Cortana" handle="@cortana32" />
